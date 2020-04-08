bf7a2be9bc21e9833a7eebec0d36e65066650960
Jim Rigsbee <jrigsbee@redhat.com>
2020-04-08 13:31:20 -0400

    WIP: more fixes for signing worker CSRs

--- 03-install_openshift/roles/do_ocp_install/files/sign.sh
+++ 03-install_openshift/roles/do_ocp_install/files/sign.sh
@@ -2,11 +2,10 @@
 
 while :
 do
-  if [ $(oc get csr -ojson | jq -r '.items[] | select(.status == {} ) | .metadata.name' | wc -l) -eq 0 ]; then
-    break
-  fi
   for csr in $(oc get csr -ojson | jq -r '.items[] | select(.status == {} ) | .metadata.name'); do
     oc adm certificate approve ${csr}
   done
-  sleep 20
+  if [ $(oc get csr | grep worker | grep Approved | wc -l) -eq $1 ]; then
+    break
+  fi
 done
